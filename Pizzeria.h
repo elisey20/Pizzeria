d8d4db56b0c86960a108a23f421da66bad18ed11
Elisey <elisey.li.2002@mail.ru>
2021-04-28 14:31:19 +1000

    init

---
+++ Pizzeria.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Pizza
+{
+private:
+    string name;
+    string description;
+    unsigned short size;
+    unsigned short price;
+
+public:
+
+};
+
+class Order
+{
+private:
+    vector<Pizza> order;
+
+public:
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "Pizzeria.h"
+
+int main()
+{
+
+    return 0;
+}
