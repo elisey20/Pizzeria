f74283c64328c32a1ccc5920ba81cf09d5793d8c
elisey20 <elisey.li.2002@mail.ru>
2021-05-08 12:52:55 +1000

    v1.0.2

--- Pizzeria.h
+++ Pizzeria.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 #include <vector>
 #include <exception>
+#include <stdexcept>
 
 using namespace std;
-
-class Order;
 
 class Pizza
 {
@@ -16,54 +16,116 @@ private:
     unsigned short size;
     unsigned short salt = 0;
     unsigned short cheese = 0;
+    unsigned short priceOnSize;
     unsigned short price;
-    Pizza(string name, string description, unsigned short size, unsigned short price)
+    Pizza(string name, string description, unsigned short priceOnSize)
     {
         this->name = move(name);
         this->description = move(description);
         this->size = size;
-        this->price = price;
+        this->priceOnSize = priceOnSize;
     }
 
-    static unsigned short checkSize(unsigned short size, unsigned short priceOnSize)
+    unsigned short checkSize(unsigned short size)
     {
         if (!(size >= 25 && size <= 40 && size % 5 == 0))
             throw logic_error("Неверный размер пиццы. Пожалуйста, укажите один из вариантов: 25, 30, 35, 40.");
         return priceOnSize * size;
     }
 
+protected:
+    Pizza() = default;
+
 public:
-    static Pizza createBravo(unsigned short size)
+    //virtual ~Pizza() = default;
+
+    static const vector<string> pizzas;
+
+    static Pizza createBravo()
     {
-        unsigned short price = checkSize(size, 20);
-        return Pizza("Браво", "Острое блюдо. Соус томатный, бекон домашний, фарш говяжий, лук, перец сладкий, лук, томаты, острый соус, сыр моцарелла.", size, price);
+        return Pizza(pizzas[0], "Острое блюдо. Соус томатный, бекон домашний, фарш говяжий, лук, перец сладкий, лук, томаты, острый соус, сыр моцарелла.",
+                     20);
     }
 
-    static Pizza createVirginia(unsigned short size)
+    static Pizza createVirginia()
     {
-        unsigned short price = checkSize(size, 20);
-        return Pizza("Вирджиния", "Острое блюдо. Соус томатный, острый соус, сыр моцарелла, колбаски охотничьи, перец болгарский, кукуруза, томаты, бекон.", size, price);
+        return Pizza(pizzas[1], "Острое блюдо. Соус томатный, острый соус, сыр моцарелла, колбаски охотничьи, перец болгарский, кукуруза, томаты, бекон.",
+                     20);
     }
 
-    void addSalt(unsigned short amt)
+    virtual void chooseSizeOfPizza(unsigned short size)
+    {
+        this->size = checkSize(size);
+    }
+
+    virtual void addSalt(unsigned short amt)
     {
         salt += amt;
     }
 
-    void addCheese(unsigned short amt)
+    virtual void addCheese(unsigned short amt)
     {
         cheese += amt;
         price += 20 * amt;
     }
 
+    virtual void print()
+    {
+
+    }
+
+    virtual void getListOfPizzas() {};
+
 };
+
+const vector<string> Pizza::pizzas = {"Браво", "Вирджиния"};
 
 class Order : public Pizza
 {
 private:
     vector<Pizza> order;
 
-public:
+    void addSalt(unsigned short amt) override {}
+    void addCheese(unsigned short amt) override {}
 
+public:
+    Order() = default;
+    //~Order() override = default;
+
+    void print() override
+    {
+        for (auto & pizza : order)
+            pizza.print();
+    }
+
+    void getListOfPizzas() override
+    {
+        for (int i = 0; i < pizzas.size(); i++)
+            cout << i + 1 << ": " << pizzas[i] << endl;
+    }
+
+    void chooseSizeOfPizza(unsigned short size) override
+    {
+        if (!order.empty())
+            order.back().chooseSizeOfPizza(size);
+        else
+            throw logic_error("Список заказа пуст!");
+    }
+
+    void addToOrder(int num)
+    {
+        if (num < 0 || num > pizzas.size())
+            throw logic_error("Неверная цифра!");
+
+        switch (num)
+        {
+            case 1:
+                order.push_back(Pizza::createBravo());
+                break;
+            case 2:
+                order.push_back(Pizza::createVirginia());
+                break;
+        }
+    }
 
 };
--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 
 #include "Pizzeria.h"
 
+using namespace std;
+
 void makeOrder()
 {
     Order order;
 
+    cout << "Пожалуйста, выберете пиццу(1-" << Pizza::pizzas.size() << "):" << endl;
+    order.getListOfPizzas();
+
+    int selectionNumber;
+    cin >> selectionNumber;
+    //добавить try catch
+    order.addToOrder(selectionNumber);
+
+    bool flag;
+    do
+    {
+        try
+        {
+            cout << "Укажите размер пиццы(25, 30, 35, 40):" << endl;
+            cin >> selectionNumber;
+            order.chooseSizeOfPizza(selectionNumber);
+            flag = false;
+        }
+        catch (logic_error& error)
+        {
+            cout << error.what() << endl;
+            flag = true;
+        }
+    } while (flag);
 }
 
 int main()
